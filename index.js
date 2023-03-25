8b0615745122517b113ff812d132fe32e4965756
lewis-otwori <lewisotwori@gmail.com>
2023-03-25 16:15:08 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then(data => renderBooks(data))
+  
   
 }
 
@@ -7,7 +11,7 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
